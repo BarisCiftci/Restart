9cfae965c8bd4b591ea400ff2c556c515334f434
Baris Ciftci <barishciftci@gmail.com>
2022-09-16 22:17:34 +0200

    Fist setting has been done

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Baris Ciftci on 16/09/2022.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: - Property
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    //MARK: - Body
+    var body: some View {
+        VStack(spacing: 20){
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action:{
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        } //: VSTACK
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
