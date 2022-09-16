7d0d6a7ed16c4ce98854e76f8fe909373c57afe4
Baris Ciftci <barishciftci@gmail.com>
2022-09-16 21:25:26 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Baris Ciftci on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
