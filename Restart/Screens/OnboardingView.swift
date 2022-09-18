f80039ee1edc3a4f769b61a9facdf513e24592f5
Baris Ciftci <barishciftci@gmail.com>
2022-09-18 20:47:09 +0200

    OnboardingView: Image has been added, All other UI elements have been added and updated.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,25 +12,118 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
-    //MARK: - Body
+    //MARK: - BODY
     var body: some View {
-        VStack(spacing: 20){
-            Text("Onboarding".uppercased())
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(.pink)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action:{
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VSTACK
+            VStack(spacing: 20){
+                //MARK: HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's now how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                }//: HEADER
+                
+                //MARK: CENTER
+                
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                       
+                    }//: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }//: CENTER
+                
+                Spacer()
+                //MARK: FOOTER
+                
+                ZStack{
+                    //: PARTS OF CUTOM BUTTON
+                    
+                    //: 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                   
+                    //: 2. CALL-TO-ACTION (STATIC)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    //: 3. CAPSULE (DYNAMIC WICTH)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                        
+                    
+                    
+                    //: 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .onTapGesture {
+                        isOnboardingViewActive = false
+                    }
+                        
+                        Spacer()
+                    }//: HSTACK
+                    
+                    Spacer()
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
+            .previewDevice("iPhone 13")
     }
 }
